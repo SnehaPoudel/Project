c7291a28d9c8514f453e01c7d4c9da3a66686f9d
Sneha <sneha.poudel@deerwalk.edu.np>
2025-02-11 10:32:08 +0545

    completed subscribe modal

--- src/components/Buttons/index.tsx
+++ src/components/Buttons/index.tsx
@@ -18,13 +18,12 @@ const Button: React.FC<ButtonProps> = ({
 }) => {
   const baseStyles =
     "inline-flex items-center gap-[6px] font-medium transition-all duration-300 rounded-[6px] leading-[20px] font-[500]";
-
   // Variants
   const variants = {
-    primary: "bg-[#1D1D22] px-[10px] py-[6px] min-w-[152px] h-[42px] hover:bg-[#333333] text-[#FFF] justify-center", 
+    primary: "bg-[#1D1D22] p-[11.07px] min-w-[152px] h-[36px] hover:bg-[#333333] text-[#FFF] text-[14.654px] justify-center", 
     secondary: "bg-[#007AFF1A] px-[10px] py-[6px] min-w-[121px] h-[32px] text-[#007AFF] hover:bg-[#007AFF33] justify-center",
     outline: "border-2 border-[#1D1D22] text-[#1D1D22] px-[10px] py-[6px] bg-transparent hover:bg-[#1D1D22] hover:text-[#FFF] min-w-[152px] h-[32px] flex items-center justify-center rounded-[6px]",
-    text: "bg-transparent text-[#007AFF] hover:text-[#005BB5] hover:underline p-0 m-0 h-auto justify-center whitespace-nowrap", 
+    text: "bg-transparent text-[#52A5FF] hover:text-[#005BB5] hover:underline p-0 m-0 h-auto justify-center whitespace-nowrap", 
     fullWidth: "bg-[#1D1D22] px-[10px] py-[14px] w-full min-w-[152px] h-[48px] justify-center items-center rounded-[10px] gap-[10px] flex hover:bg-[#333333] text-[#FFF]",
     iconbutton: "w-[29px] h-[29px] flex-shrink-0 flex items-center justify-center rounded-[50%]  " 
   };
--- src/components/SubscribeModal.tsx
+++ src/components/SubscribeModal.tsx
@@ -12,18 +12,18 @@ export default function SubscribeModal() {
             }}
             >
             <div className="w-1/2 h-full flex flex-col justify-content: flex-end; p-6 ml-auto">
-                <div className="absolute top-4 right-4">
+                <div className="absolute top-2 right-2">
                     <Button variant="iconbutton" customIcon={<ImageIcon/>} children={undefined}>
                     </Button>
                 </div>
-                <div className="flex flex-col pr-[17px] gap-[14px] mt-10">
+                <div className="flex flex-col pr-[17px] gap-[14px] mt-9">
                     
                         <div>
-                            <h2 className="text-2xl text-[#232426] font-bold text-[22px]">Subscribe</h2>
+                            <h2 className="text-2xl text-[#232426] font-inter text-[22px] font-semibold leading-normal">Subscribe</h2>
 
                         </div>
                         <div>
-                            <p className="text-[#7B828C] font-normal text-[13px] font-normal leading-normal ">
+                            <p className="text-[#7B828C] font-inter text-[13px] font-normal leading-[17px] ">
                                 Be the first to receive our latest product updates, newest
                                 offerings, and free product trials.
                             </p>
@@ -32,23 +32,23 @@ export default function SubscribeModal() {
                     <input
                         type="email"
                         placeholder="Email Address"
-                        className="w-full px-4 py-2 border rounded-[3px] bg-[#D7E0EB] text-[#7B828C] placeholder-gray-500"
+                        className="w-full h-[36px] px-[8px] py-[9px] border rounded-[3px] bg-[#D7E0EB] text-[#7B828C] placeholder-gray-500 placeholder:text-sm"
                     />
             
-                    <Button variant="primary" className="p-6  w-full h-[42px] ">
+                    <Button variant="primary" className="p-6  w-full h-[36px] ">
                             Continue with Email
                     </Button>
                     
                     
                     <div className="text">
-                        <p className="text-[11px] text-gray-500">
+                        <p className="text-[#7B828C] font-inter text-[11px] font-normal leading-[5px] my-[2px] py-[5px]">
                             You can learn more about how we handle user data through the  
                             <span className="ml-1">
-                                <Button variant="text" className="text-[#52A5FF] font-inter text-[11px] font-normal leading-normal " >
-                                    Walcy Privacy Policy.
+                                <Button variant="text" className="text-[#52A5FF]  text-[11px] leading-normal " >
+                                    Walcy Privacy Policy
                                 </Button>
                             </span>
-                            
+                            .
                         </p>
                     </div>
                 </div>
